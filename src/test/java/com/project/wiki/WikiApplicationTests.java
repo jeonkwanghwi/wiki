16c80c6a614cbb90b0d8936bb6d45cb72a553bfc
kwanghwi <nimo6289@gmail.com>
2024-01-24 23:55:29 +0900

    Question 컨트롤러 수정

--- src/test/java/com/project/wiki/WikiApplicationTests.java
+++ src/test/java/com/project/wiki/WikiApplicationTests.java
@@ -1,5 +1,6 @@
 package com.project.wiki;
 
+import com.project.wiki.entity.Answer;
 import com.project.wiki.entity.Question;
 import com.project.wiki.repository.QuestionRepository;
 import org.junit.jupiter.api.Test;
@@ -10,6 +11,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -21,28 +23,22 @@ class WikiApplicationTests {
     private QuestionRepository questionRepository;
 
     @Test
+    // Transactional이 없어지니까 오히려 값이 들어간다 ?
     void testJpa() {
         Question q1 = new Question().builder()
-                .subject("sbb가 무엇인가요?")
-                .content("sbb에 대해서 알고 싶습니다.")
+                .subject("sbb가 무엇인가요 1")
+                .content("sbb에 대해서 알고 싶습니다.1")
                 .createDate(LocalDateTime.now())
                 .build();
         questionRepository.save(q1); // 첫번째 질문 저장
 
 
-        Optional<Question> oq = this.questionRepository.findById(1);
-        assertTrue(oq.isPresent());
-        Question q = oq.get();
+        Question q2 = new Question().builder()
+                .subject("sbb가 무엇인가요 2")
+                .content("sbb에 대해서 알고 싶습니다.2")
+                .createDate(LocalDateTime.now())
+                .build();
+        questionRepository.save(q2); // 두번째 질문 저장
 
-        q1.builder().subject("수정제목");
-        this.questionRepository.save(q1);
-        System.out.println(q1.getSubject());
-
-//        Question q2 = new Question();
-//        q2.setSubject("스프링부트 모델 질문입니다.");
-//        q2.setContent("id는 자동으로 생성되나요?");
-//        q2.setCreateDate(LocalDateTime.now());
-//        this.questionRepository.save(q2);  // 두번째 질문 저장
     }
-
 }
