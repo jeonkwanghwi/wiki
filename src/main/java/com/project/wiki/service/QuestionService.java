8702041139528da8fc2bf01c62d1e4b841924aaf
kwanghwi <nimo6289@gmail.com>
2024-01-28 14:17:28 +0900

    question detail 추가

---
+++ src/main/java/com/project/wiki/service/QuestionService.java
@@ -0,0 +1,31 @@
+package com.project.wiki.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.project.wiki.DataNotFoundException;
+import com.project.wiki.entity.Question;
+import com.project.wiki.repository.QuestionRepository;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class QuestionService {
+
+    private final QuestionRepository questionRepository;
+
+    public List<Question> getList() {
+        return this.questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id) {
+        Optional<Question> question = this.questionRepository.findById(id);
+        if (question.isPresent()) {
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
+    }
+}
